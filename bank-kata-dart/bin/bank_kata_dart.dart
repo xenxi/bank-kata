ab5ba73128f35f35a0a62c783ae35bc3eb9dd856
xenxi <antoniom.diaz.moreno@gmail.com>
2021-09-04 09:30:53 +0200

    initial proyect for dart

---
+++ bank-kata-dart/bin/bank_kata_dart.dart
@@ -0,0 +1,5 @@
+import 'package:bank_kata_dart/bank_kata_dart.dart' as bank_kata_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bank_kata_dart.calculate()}!');
+}
---
+++ bank-kata-dart/test/bank_kata_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:bank_kata_dart/bank_kata_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
