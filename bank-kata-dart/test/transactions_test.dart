70779dbe80122c7f09d10148d85ae9558a0a403e
xenxi <antoniom.diaz.moreno@gmail.com>
2021-09-05 12:36:02 +0200

    add test red: update balance when store transactions

--- bank-kata-dart/test/transactions_test.dart
+++ bank-kata-dart/test/transactions_test.dart
@@ -29,5 +29,30 @@ void main() {
       ];
       expect(transactions.getAll(), expectedTransactions);
     });
+
+    test('update balance when store transactions', () {
+      final aGivenTransactions = <Transaction>[
+        Transaction(amount: 100, date: DateTime.now(), balance: 100),
+        Transaction(
+            amount: 200,
+            date: DateTime.now().add(Duration(minutes: 2)),
+            balance: 300),
+        Transaction(
+            amount: -50,
+            date: DateTime.now().add(Duration(minutes: 5)),
+            balance: 250),
+      ];
+      final aGivenDateTime = DateTime.now().add(Duration(hours: 1));
+      when(dateTimeGetter.getCurrentDate())
+          .thenAnswer((realInvocation) => aGivenDateTime);
+
+      transactions.add(100);
+
+      final expectedTransactions = <Transaction>[
+        ...aGivenTransactions,
+        Transaction(amount: 100, date: aGivenDateTime, balance: 350)
+      ];
+      expect(transactions.getAll(), expectedTransactions);
+    });
   });
 }
